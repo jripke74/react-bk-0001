7a1c2d4e00c811f3b6214eaaf35dde9a305137b1
Jeff Ripke <jripke74@gmail.com>
2023-03-22 13:51:29 -0700

    add EmailInput.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import GoalList from './components/GoalList';
+import EmailInput from './components/EmailInput';
 
 function App() {
   return (
     <>
       <Header />
+      <EmailInput />
       <GoalList />
     </>
   );
---
+++ src/components/EmailInput.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+function EmailInput() {
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function evaluateEmail(event) {
+    const enteredEmail = event.target.value;
+    if (enteredEmail.trim() === '' || !enteredEmail.includes('@')) {
+      setErrorMessage('The entered email address is invalid.');
+    } else {
+      setErrorMessage('');
+    }
+  }
+
+  return (
+    <div>
+      <input placeholder="Your email" type="email" onBlur={evaluateEmail} />
+      <p>{errorMessage}</p>
+    </div>
+  );
+}
+
+export default EmailInput;
