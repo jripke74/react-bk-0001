8f82a91ed842e4d7ca1f09c102d62964a8187c93
Jeff Ripke <jripke74@gmail.com>
2023-03-24 13:33:11 -0700

    add ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>March 11th 1974</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$284.33</div>
+        <div className="expense-item__price">$284.33</div>
       </div>
     </div>
   );
