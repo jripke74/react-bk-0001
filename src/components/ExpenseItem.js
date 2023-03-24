6ee6f13119c8fcf14fcb36a925ab21d7a54fa39a
Jeff Ripke <jripke74@gmail.com>
2023-03-24 10:19:04 -0700

    add expense details

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,13 @@
 function ExpenseItem() {
   return (
-    <h2>Expense item!</h2>
+    <div>
+      <div>March 11th 1974</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$284.33</div>
+      </div>
+    </div>
   );
-};
+}
 
 export default ExpenseItem;
