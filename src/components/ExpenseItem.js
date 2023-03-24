e057f53092903c6ee3757d0a83878e847c42ae75
Jeff Ripke <jripke74@gmail.com>
2023-03-24 14:19:55 -0700

    add dynamic data to expenseitem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 11);
+  const expenseTitle = 'Car Insuranc';
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>March 11th 1974</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$284.33</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
