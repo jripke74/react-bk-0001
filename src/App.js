b5e1f93c037b70f3abaa64f2d81ca413459611dc
Jeff Ripke <jripke74@gmail.com>
2023-03-25 16:30:27 -0700

    add UI and Expenses folders in Components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import GoalList from './components/GoalList';
 import EmailInput from './components/EmailInput';
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
