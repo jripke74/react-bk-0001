c4bbd825598a8e0286eebde3ad39f804b0100ee4
Jeff Ripke <jripke74@gmail.com>
2023-04-29 07:44:54 -0700

    add year filter

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,19 +1,23 @@
 import './ExpensesFilter.css';
 
-const ExpenseFilter = () => {
+const ExpensesFilter = (props) => {
+  const dropChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+
   return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
+        <select value={props.selected} onChange={dropChangeHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
         </select>
       </div>
     </div>
-  )
+  );
 };
 
-export default ExpenseFilter;
+export default ExpensesFilter;
