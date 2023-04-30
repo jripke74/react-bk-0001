4f574c2a076cf173b64c623c520f58ba7448332c
Jeff Ripke <jripke74@gmail.com>
2023-04-30 12:37:25 -0700

    add stateful list for expenses

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import './App.css';
 import Header from './components/Header';
 import GoalList from './components/GoalList';
@@ -5,32 +7,35 @@ import EmailInput from './components/EmailInput';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Pager',
+    amount: 94.12,
+    date: new Date(2021, 2, 12),
+  },
+  { id: 'e2', title: 'TV', amount: 799.99, date: new Date(2021, 7, 14) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'desk',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Pager',
-      amount: 94.12,
-      date: new Date(2021, 2, 12),
-    },
-    { id: 'e2', title: 'TV', amount: 799.99, date: new Date(2021, 7, 14) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'desk',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
