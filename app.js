180ba175fa6ce41bbb1ee4bcdd2584e0269b068e
Jeff Ripke <jripke74@gmail.com>
2023-06-02 08:22:42 -0700

    add parsing

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('mesage.txt', 'DUMMY');
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split('=')[1];
+      fs.writeFileSync('mesage.txt', message);
+    });
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
