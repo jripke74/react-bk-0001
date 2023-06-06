fcaa21fa467d1f17e00fb2c7e472199e027acdab
Jeff Ripke <jripke74@gmail.com>
2023-06-06 07:55:17 -0700

    add util/path.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(
-    path.join(__dirname, '..', 'src', 'components', 'add-product.html')
-  );
+  res.sendFile(path.join(rootDir, 'src', 'components', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
