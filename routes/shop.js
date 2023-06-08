f3f1b131bcdc8b868e868aff243f22479eec181f
Jeff Ripke <jripke74@gmail.com>
2023-06-08 11:37:46 -0700

    add includes/

--- routes/shop.js
+++ routes/shop.js
@@ -3,13 +3,17 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
-
-const router = express.Router();
 const adminData = require('./admin');
 
+const router = express.Router();
+
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.render('shop', { pageTitle: 'Shop', prods: adminData.products });
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+  });
 });
 
 module.exports = router;
