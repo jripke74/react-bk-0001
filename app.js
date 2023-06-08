274785aef63ef1b23e1b149714d0e3f122eaeeb9
Jeff Ripke <jripke74@gmail.com>
2023-06-08 10:59:14 -0700

    add ejs

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'src/components');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -15,9 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, 'src', 'components', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3001);
